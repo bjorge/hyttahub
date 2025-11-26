857ad7e2e648bf6097ffd9ac5776f6e2e1c0f7f8
William Bjorge <bjorge@Williams-Mac-mini.local>
2025-11-26 15:18:19 -0800

    Changed site name in account screen to update view first.

--- lib/site_widgets/site_name_widget.dart
+++ lib/site_widgets/site_name_widget.dart
@@ -7,19 +7,31 @@ import 'package:hyttahub/site_blocs/site_replay_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SiteNameDisplay extends StatelessWidget {
+class SiteNameDisplay extends StatefulWidget {
   final String collectionName;
 
   const SiteNameDisplay({super.key, required this.collectionName});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      key: Key("SiteNameDisplay-$collectionName"),
+  State<SiteNameDisplay> createState() => _SiteNameDisplayState();
+}
 
-      create: (_) =>
-          SiteReplayBloc(collectionName)
-            ..add(CommonReplayBlocEvent(listen: true)),
+class _SiteNameDisplayState extends State<SiteNameDisplay> {
+  late final SiteReplayBloc bloc = SiteReplayBloc(widget.collectionName);
+
+  @override
+  void dispose() {
+    bloc.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider.value(
+      value: bloc,
+      key: Key("SiteNameDisplay-${widget.collectionName}"),
+
+      // create: (_) => SiteReplayBloc(widget.collectionName),
       child: BlocSelector<SiteReplayBloc, SiteReplayBlocState, String>(
         selector: (state) => state.name,
         builder: (context, name) {
@@ -31,5 +43,14 @@ class SiteNameDisplay extends StatelessWidget {
         },
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      bloc.add(CommonReplayBlocEvent(listen: true));
+    });
   }
 }
