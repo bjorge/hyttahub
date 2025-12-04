a9f7df26b27f21bad69324925bd2b7759acc33a2
William Bjorge <bjorge@Williams-Mac-mini.local>
2025-12-04 14:06:47 -0800

    Removed unused localization keys.

--- examples/formproto/lib/l10n/app_localizations_es.dart
+++ examples/formproto/lib/l10n/app_localizations_es.dart
@@ -9,15 +9,6 @@ class AppLocalizationsEs extends AppLocalizations {
   AppLocalizationsEs([String locale = 'es']) : super(locale);
 
   @override
-  String get app_createAlbumTitle => 'Crear Álbum';
-
-  @override
-  String get app_albumNameLabel => 'Nombre del Álbum';
-
-  @override
-  String get app_addPhotosTitle => 'Añadir Fotos';
-
-  @override
   String app_uploadingPhotosProgress(Object count, Object total) {
     return 'Subiendo $count de $total archivos...';
   }
@@ -28,153 +19,13 @@ class AppLocalizationsEs extends AppLocalizations {
   }
 
   @override
-  String get app_pickPhotosButton => 'Seleccionar Fotos';
-
-  @override
-  String get app_selectPhotosToUpload => 'Selecciona fotos para subir:';
-
-  @override
-  String get app_hideCaptionTooltip => 'Ocultar Leyenda';
-
-  @override
-  String get app_showCaptionTooltip => 'Mostrar Leyenda';
-
-  @override
-  String get app_noPhotos => 'no hay fotos';
-
-  @override
-  String get app_noAlbums => 'no hay álbumes';
-
-  @override
-  String get app_updateCaption => 'Actualizar Leyenda';
-
-  @override
-  String get app_reorderAlbumsTitle => 'Reordenar Álbumes';
-
-  @override
-  String get app_renameAlbumTitle => 'Renombrar Álbum';
-
-  @override
-  String get app_reorderPhotos => 'Reordenar Fotos';
-
-  @override
-  String get app_photoInfoTitle => 'Información de la Foto';
-
-  @override
-  String get app_photoNotFound => 'Foto no encontrada.';
-
-  @override
-  String get app_albumLabel => 'Álbum';
-
-  @override
-  String get app_notAvailable => 'N/D';
-
-  @override
-  String get app_originalNameLabel => 'Nombre Original';
-
-  @override
-  String get app_sizeLabel => 'Tamaño';
-
-  @override
-  String get app_siteSettingsTitle => 'Configuración del Sitio';
-
-  @override
-  String get app_addAlbumTitle => 'Añadir Álbum';
-
-  @override
-  String get app_newAlbumNameLabel => 'Nombre del Nuevo Álbum';
-
-  @override
-  String get app_addPhotoCaptionTitle => 'Añadir Leyenda a la Foto';
-
-  @override
-  String get app_photoCaptionLabel => 'Leyenda de la Foto';
-
-  @override
-  String get app_deletePhotoTitle => 'Eliminar Foto';
-
-  @override
-  String get app_deletePhotoConfirmation =>
-      '¿Eliminar Foto? Esta acción no se puede deshacer.';
-
-  @override
-  String get app_deleteAlbumTitle => 'Eliminar Álbum';
-
-  @override
-  String get app_deleteAlbumConfirmation =>
-      '¿Eliminar Álbum? Esta acción no se puede deshacer y eliminará todos los miembros y fotos del álbum.';
-
-  @override
-  String get app_albumNotFound => 'Álbum no encontrado';
-
-  @override
-  String get app_albumNameEmptyError =>
-      'El nombre del álbum no puede estar vacío.';
-
-  @override
   String app_photoSizeInKB(String size) {
     return '$size KB';
   }
 
   @override
-  String get app_unknownAlbum => 'Álbum Desconocido';
-
-  @override
-  String get app_eventsTitle => 'Eventos de la Aplicación';
-
-  @override
-  String get app_stateTitle => 'Estado de la Aplicación';
-
-  @override
-  String get app_showAppEventsState =>
-      'Mostrar Eventos y Estado de la Aplicación';
-
-  @override
-  String get app_errorTitle => 'Error';
-
-  @override
   String get app_unexpectedError =>
       'Error inesperado, comprueba tu conexión a internet, vuelve atrás e inténtalo de nuevo';
-
-  @override
-  String get app_accessDeniedTitle => 'Acceso Denegado';
-
-  @override
-  String get app_sitePermissionDenied =>
-      'No tienes permiso para ver este sitio. Contacta al administrador del sitio si tu correo electrónico debería estar permitido.';
-
-  @override
-  String get app_networkError =>
-      'Ocurrió un error de red. Por favor, comprueba tu conexión.';
-
-  @override
-  String get app_editModeTitle => 'Modo Edición';
-
-  @override
-  String get app_adminPrivileges =>
-      'Tienes privilegios de administrador para este sitio.';
-
-  @override
-  String get app_howToProceed => '¿Cómo te gustaría proceder?';
-
-  @override
-  String get app_viewSite => 'Ver Sitio';
-
-  @override
-  String get app_editSite => 'Editar Sitio';
-
-  @override
-  String get app_renameSiteTitle => 'Renombrar Sitio';
-
-  @override
-  String get app_manageSiteMembers => 'Gestionar Miembros del Sitio';
-
-  @override
-  String get app_showSiteEventsState => 'Mostrar Eventos y Estado del Sitio';
-
-  @override
-  String get app_showSiteAllowedEmails =>
-      'Mostrar Correos Permitidos del Sitio';
 
   @override
   String get app_appTitle => 'Hytta Hub Álbumes';
@@ -213,12 +64,6 @@ class AppLocalizationsEs extends AppLocalizations {
 
   @override
   String get app_serviceLoginButton => 'Inicio de Sesión de Servicio';
-
-  @override
-  String get app_exportSiteTitle => 'Exportar Sitio';
-
-  @override
-  String get app_manageExportsTitle => 'Gestionar Exportaciones';
 
   @override
   String get app_updateTextButton => 'Actualizar Texto';
