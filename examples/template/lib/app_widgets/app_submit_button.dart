6e1497649012f49b18b0c3b797d1f4633838d851
William Bjorge <bjorge@Williams-Mac-mini.local>
2026-02-05 01:56:53 -0800

    Added version detection to app submission buttons.

--- examples/template/lib/app_widgets/app_submit_button.dart
+++ examples/template/lib/app_widgets/app_submit_button.dart
@@ -1,8 +1,7 @@
 // Copyright (c) 2025 bjorge
 
-import 'package:template/app_blocs/app_submit_bloc.dart';
+import 'package:template/app_blocs/app_replay_bloc.dart';
 import 'package:hyttahub/common_blocs/base_submit_bloc.dart';
-import 'package:template/proto/app_events.pb.dart';
 import 'package:hyttahub/proto/common_blocs.pb.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,26 +13,33 @@ class AppSubmitIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AppSubmitBloc, BaseSubmitState<SubmitAppEvent>>(
-      builder: (context, submitState) {
-        return IconButton(
-          icon: const Icon(Icons.check),
-          onPressed:
-              (submitState.submissionState.state !=
-                      CommonSubmitBlocState_State.canSubmit)
+    return BlocBuilder<AppReplayBloc, AppReplayBlocState>(
+      builder: (context, replayState) {
+        return BlocBuilder<AppSubmitBloc, BaseSubmitState<SubmitAppEvent>>(
+          builder: (context, submitState) {
+            final isOutdated = submitState.payload != null &&
+                replayState.lastVersion >= submitState.payload!.siteEvent.version;
+
+            return IconButton(
+              icon: Icon(isOutdated ? Icons.block : Icons.check),
+              onPressed: (submitState.submissionState.state !=
+                          CommonSubmitBlocState_State.canSubmit ||
+                      isOutdated)
                   ? null
                   : () {
-                    if (formKey.currentState!.validate()) {
-                      formKey.currentState!.save();
-                      context.read<AppSubmitBloc>().add(
-                        AppEventSubmission(
-                          submission: CommonSubmitBlocEvent(
-                            submit: CommonSubmitBlocEvent_SubmitNow(),
-                          )..freeze(),
-                        ),
-                      );
-                    }
-                  },
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState!.save();
+                        context.read<AppSubmitBloc>().add(
+                              AppEventSubmission(
+                                submission: CommonSubmitBlocEvent(
+                                  submit: CommonSubmitBlocEvent_SubmitNow(),
+                                )..freeze(),
+                              ),
+                            );
+                      }
+                    },
+            );
+          },
         );
       },
     );
